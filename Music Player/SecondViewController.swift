63299217982aef9d58ce69055ce1f802b10a704e
McG <chrismcgilbert@gmail.com>
2020-07-07 09:20:45 -0700

    Initial Commit

---
+++ Music Player/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Music Player
+//
+//  Created by Christopher McCarroll-Gilbert on 7/7/20.
+//  Copyright © 2020 Christopher McCarroll-Gilbert. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
